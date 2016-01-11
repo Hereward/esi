512a7eefaabbc3f762a3a653a05ec37a4902ae93
Hereward <hereward@planetonline.com.au>
2016-01-11 12:17:22 +0700

    Added features section to Product page

--- site/templates/products_tpl.php
+++ site/templates/products_tpl.php
@@ -48,6 +48,32 @@
 </div>
 
 
+<div class="container-fluid">
+    <div class="featurettes products">
+        <div class="row featurette grey">
+            <div class="sub-container">
+                <div class="col-xs-12 col-sm-6">
+                    <img class="img-responsive" src="<?=$page->policy_features_image->url?>" alt="policy features image">
+                </div>
+
+                <div class="col-xs-12 col-sm-6">
+                        <div class="featurette_right_content">
+                            <h2 class="featurette-heading"><?=$page->policy_features_heading?></h2>
+                            <?=$page->policy_features_text?>
+
+                        </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 
 
 
